6b19318ff8cda5183a49fca8bca66379119a0c49
Cynthia Dahl <cdahl@Cynthias-MacBook-Pro.local>
2023-12-18 20:56:06 -0500

    Fixing keyword search bug when across chunks

--- utils.py
+++ utils.py
@@ -7,7 +7,8 @@ DEFAULT_EVENT_COUNT = 1000
 def get_log_events(filename, **kwargs):
     filepath = f"{LOG_FOLDER}/{filename}"
     file_size = os.stat(filepath).st_size
-    chunk_size = min(int(1000 * 1024), file_size)
+    chunk_size = min(int(2000 * 1024), file_size)
+    
     result = []
     n = DEFAULT_EVENT_COUNT
     keyword = "" # support only one for now
@@ -18,27 +19,39 @@ def get_log_events(filename, **kwargs):
     if 'keyword' in kwargs:
         keyword = kwargs['keyword']
 
+    def has_match(input):
+        return not keyword or keyword in input
+
     def process_line(line):
-        if not keyword or keyword in line:
+        if has_match(line):
             result.append(line)
 
     def parse_lines(input, buffer):
-        if keyword and not keyword in input:
-            return ""
-
         is_first = True
         last_index = len(input)
+        input_match = has_match(input)
         index = input.rfind("\n")
-        while index >= 0 and len(result) < n:
-            line = input[index+1:last_index]
-            if not is_first:
-                process_line(line)
-            else:
-                process_line(line + buffer)
-                is_first = False
-            
-            last_index = index
-            index = input.rfind("\n", 0, index)
+ 
+        # edge case: a match for the keyword could be broken up across chunks
+        if not input_match and buffer:
+            input_match = index >= 0 and has_match(input[index+1:last_index] + buffer)
+
+        if input_match:
+            while index >= 0 and len(result) < n:
+                line = input[index+1:last_index]
+                if not is_first:
+                    process_line(line)
+                else:
+                    process_line(line + buffer)
+                    is_first = False
+                
+                last_index = index
+                index = input.rfind("\n", 0, index)
+        else:
+            # no match, but we still may have buffer content
+            last_index = input.find("\n")
+            if last_index < 0:
+                last_index = len(input)
 
         if last_index > 0:
             extra_buffer = (buffer if is_first else "")
